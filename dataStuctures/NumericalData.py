97cee5867eb5b4665c94da924d4c4304256a1dc8
MathGravel <mathieu.gravel.officiel@gmail.com>
2024-12-18 12:34:03 -0500

    Fix rapide avant de devoir partir

--- dataStuctures/NumericalData.py
+++ dataStuctures/NumericalData.py
@@ -4,6 +4,7 @@ from fastapi import UploadFile, File
 import io
 import json
 from queue import PriorityQueue
+import functools
 
 chunk_file_size = 100000
 process_folder = './__processFiles__'
@@ -36,6 +37,7 @@ class NumericalStructure:
     def __init__(self,global_instance:bool=False,file_path:str=None):
         self.global_instance = global_instance
         self.data = []
+        self.chunkData= False
         if not file_path is None:
             file = open(file_path, "r")
             size = os.path.getsize(file_path)
@@ -53,15 +55,20 @@ class NumericalStructure:
     def _process_data(self,file: io.TextIOWrapper = None,fileSize:bytes = 0) -> None:
         if fileSize < chunk_file_size:
             self.process_entire_file(-1,file)
+            self.chunkData = False
         else:
             self.process_data_per_chunk(file)
+            self.chunkData = True
 
     def get_Data(self,n_values:int = 3,file:UploadFile=None) -> list[NumericalInstance]:
         if not self.global_instance and file is not None:
             fileReader = io.TextIOWrapper(file.file, encoding='utf-8')
             return [x.key for x in self.process_entire_file(n_values,fileReader)]
-        else:
+        elif self.chunkData:
             return [x.key for x in self.get_data_from_files(n_values)]
+        else:
+            return [x.key for x in self.data[:n_values]]
+
 
     def process_entire_file(self,n_values:int,file:io.TextIOWrapper = None) -> list[NumericalInstance]:
         heap = []
@@ -69,6 +76,7 @@ class NumericalStructure:
         for line in file:
             heapq.heappush(heap,NumericalInstance(*line.split('_'))) if (i <= n_values or n_values == -1) else heapq.heapreplace(heap,NumericalInstance(*line.split('_')))
             i += 1        
+        self.data = heap
         return heapq.nlargest(n_values, heap) if n_values != -1 else heap
 
     def process_data_per_chunk(self:int,file:io.TextIOWrapper = None) -> None:
